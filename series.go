0cf815631b96cdde9ca992722eabeb15f5c6b9d1
Benji Vesterby <benji.vesterby@gmail.com>
2019-02-17 16:27:01 -0700

    Small initial changes

--- series.go
+++ series.go
@@ -9,6 +9,7 @@ import (
 // Series for handling column level data of the data frame
 type Series struct {
 	data []interface{}
+	stype string // Expected type of data in this series
 }
 
 // Lambda function caller which will concurrently iterate over the data and execute a function literal
@@ -16,41 +17,64 @@ type Series struct {
 func (this Series) Lambda(ctx context.Context, lambda func(ctx context.Context, column interface{}))(err error) {
 	var wg = sync.WaitGroup{}
 
-	// Ensure the lambda function is not nil
-	if lambda != nil {
+	if this.data != nil {
+		// Ensure the lambda function is not nil
+		if lambda != nil {
 
-		// Iterate over each row in the series
-		for index := range this.data {
-			wg.Add(1)
+			// Iterate over each row in the series
+			for index := range this.data {
+				wg.Add(1)
 
-			select {
-			case <- ctx.Done():
-				// Break out of the loop because the context has been cancelled or timed out
-				break
-			default:
-				go func() {
-					// TODO: Add handler here for panics
-					defer wg.Done()
+				select {
+				case <- ctx.Done():
+					// Break out of the loop because the context has been cancelled or timed out
+					break
+				default:
+					go func() {
+						// TODO: Add handler here for panics
+						defer wg.Done()
 
-					// Execute the lambda function
-					lambda(ctx, this.data[index])
-				}()
+						// Execute the lambda function
+						lambda(ctx, this.data[index])
+					}()
+				}
 			}
-		}
 
-		// Wait for processing to finish
-		wg.Wait()
+			// Wait for processing to finish
+			wg.Wait()
+		} else {
+			err = errors.Errorf("nil lambda function passed to series")
+		}
 	} else {
-		err = errors.Errorf("nil lambda function passed to series")
+		err = errors.Errorf("the data is nil in the series")
 	}
 
 	return err
 }
 
-func (this Series) Min() {
-
+func (this Series) SetData(data []interface{}) {
+	this.data = data
 }
 
-func (this Series) Filter() {
+func (this Series) GetData() (data []interface{}) {
+	return this.data
+}
 
+func (this Series) Min() (err error) {
+	return err
+}
+
+func (this Series) Filter() (err error) {
+	return err
+}
+
+// Validate the series struct
+func (this Series) Validate() (valid bool) {
+
+	// Valid only if the data is not nil
+	if this.data != nil {
+		valid = true
+	}
+
+	return valid
 }
